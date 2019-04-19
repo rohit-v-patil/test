9603a1638f5a591c5a124bc02c7265e02b40fb28
rohit1727 <rprohit72@gmail.com>
2019-04-19 20:46:27 +0530

    Updates

---
+++ precache-manifest.1068738edf7a11a5f033aea588a19180.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "dea3834c72f4b477ee5d",
+    "url": "/test/static/js/main.b2423ca8.chunk.js"
+  },
+  {
+    "revision": "bc39f015226863d8f03d",
+    "url": "/test/static/js/2.685fd8c9.chunk.js"
+  },
+  {
+    "revision": "1894d3d935130170e5a75e4885ff1470",
+    "url": "/test/index.html"
+  }
+];
